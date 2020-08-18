ec50fea0469d8f74ff478a1a94ec12d6267e8ea0
sibin bhaskaran <sibinbhaskaran@sibins-MacBook-Pro.local>
2020-08-18 01:33:12 -0500

    added style to suggestion

--- src/components/SuggestionDisplay.jsx
+++ src/components/SuggestionDisplay.jsx
@@ -7,17 +7,18 @@ export default class SuggestionDisplay extends Component {
         console.log(sortedData)
         return (
             
-            <div>
+            <div class="display">
                 {
                     // this.props.events.map( eventsData =>{
                         sortedData.map(eventsData =>{
                         return (
                             
-                            <ul key={eventsData._id}>
-                                <li>Description:{eventsData.description}</li>
+                            <ul class="suggestionDisplay" key={eventsData._id}>
+                                <li class="suggestion-desc">{eventsData.description}</li>
+                                
+                                <li><img class="events-image " src={eventsData.image} alt="eventsimage" /></li>
                                 <li>Location:{eventsData.location}</li>
-                                <li><img src={eventsData.image} alt="" /></li>
-                                <li>{eventsData.user_name}</li>
+                                <li>Posted by:{eventsData.user_name}</li>
                                 <li>Posted Date:{moment(eventsData.date).format('MM/DD/YYYY')}</li>
                              
                             </ul>
